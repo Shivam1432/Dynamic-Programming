f68e0bb6699e58d60ec3487fbd4ffa1ee2fd570f
Shivam <shivam1634.cse18@chitkara.edu.in>
2021-03-06 19:46:03 +0530

    Create DPSolution.java

---
+++ Longest Common Subsequence/DPSolution.java
@@ -0,0 +1,38 @@
+class Result
+{
+  static int longestCommonSubsequence(String str1, String str2)
+  {
+     int m=str1.length();
+    int n=str2.length();
+    char[] X=str1.toCharArray();
+    char[] Y=str2.toCharArray();
+    return lcs(X,Y,m,n);
+  }
+  static int lcs(char[] X,char[] Y,int m,int n)
+  {
+    int L[][]=new int[m+1][n+1];
+    for(int i=0;i<=m;i++)
+    {
+      for(int j=0;j<=n;j++)
+      {
+        if(i==0||j==0)
+        {
+          L[i][j]=0;
+        }
+        else if(X[i-1]==Y[j-1])
+        {
+          L[i][j]=L[i-1][j-1]+1;
+        }
+        else
+        {
+          L[i][j]=max(L[i-1][j],L[i][j-1]);
+        }
+      }
+    }
+    return L[m][n];
+  }
+  static int max(int a,int b)
+  {
+    return a>b?a:b;
+  }
+}
